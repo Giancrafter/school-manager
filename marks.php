70127e10e1213ba36e68a9a376cac88cab780b95
Gian Klug <gian.klug@ict-scouts.ch>
2020-12-31 22:45:37 +0100

    Added Edit and Remove, Semesters

--- marks.php
+++ marks.php
@@ -3,6 +3,38 @@ include("config.php");
 include("navbar.php");
 include("db_connect.php");
 
+//Get Semester related things
+$semester_q = $con->prepare("SELECT semesters, start FROM classes WHERE name = ?");
+$semester_q->bind_param("s", $_SESSION["class"]);
+$semester_q->execute();
+$semester_q->bind_result($semesters, $class_start);
+$semester_q->fetch();
+$semester_q->close();
+
+//Function for counting the formated Semester up
+function countUp($formatted){
+    $parts = explode("/", $formatted);
+    if ($parts[1] == 2) {
+        $parts[1] = 1;
+        $parts[0]++;
+    } else {
+        $parts[1]++;
+    }
+    return $parts[0]."/".$parts[1];
+}
+
+//get Semester
+if (date('n')==1){
+    $currentSemester = (date('Y')-1)."/";
+} else {
+    $currentSemester = date('Y')."/";
+}
+if ((date('n')>1)&&(date('n')<8)){
+    $currentSemester.="1";
+} else {
+    $currentSemester.="2";
+}
+
 //Function for getting Rank
 function getRank($g_array, $g_value){
 arsort($g_array);
@@ -13,9 +45,13 @@ while ($g_value <= $g_array[$position]) {
 return $position+1;
 }
 
+//Generate row for a subject
 function generateRow($subject) {
     global $lang;
     global $con;
+    global $semesters;
+    global $class_start;
+    global $currentSemester;
     //Set Select and Mark variables
     $add_select = "";
     $mark_table = "";
@@ -46,7 +82,7 @@ function generateRow($subject) {
             $class_marks=$result_exams["mark"];
             $ranking = array();
             $s_subject = $subject;
-            $sql_class = $con->prepare("SELECT exam, mark FROM marks WHERE class = ? AND exam = ? AND subject  = ? AND username <> ?");
+            $sql_class = $con->prepare("SELECT id, exam, mark, weight FROM marks WHERE class = ? AND exam = ? AND subject  = ? AND username <> ?");
             $sql_class->bind_param("ssss", $_SESSION["class"], $exam_single["name"], $s_subject, $_SESSION["username"]);
             $sql_class->execute();
             $sql_class_result=$sql_class->get_result();
@@ -60,12 +96,88 @@ function generateRow($subject) {
             $class_avg = round($class_marks/$class_count, 2);
             $pos_in_class=getRank($ranking, $result_exams["mark"]);
             $sql_class->close();
+            
+            //Semester Select
+            $sme = $class_start;
+            for ($q = 1; $q <= $semesters; $q++){
+                if($sme==$currentSemester) {$selected="selected";} else {$selected="";}
+                $semester_select .= "<option value=\"{$sme}\" $selected>{$sme}</option>";
+                $sme = countUp($sme);
+            }
+
             $mark_table .= <<< EOT
                 <tr> 
                     <td>{$result_exams["exam"]}</td>
                     <td>{$result_exams["mark"]}</td>
                     <td>$class_avg</td>
                     <td>$pos_in_class/{$class_count}</td>
+                    <td>            
+                        <form id="{$result_exams["id"]}" style="display: inline;">
+                        <input type="hidden" name="id" value="{$result_exams["id"]}"><input type="hidden" name="remove"></input><input type="hidden" name="subject" value="$subject"></input>
+                        </input>
+                        <button class="" style="color:white; background-color: black; padding:5px; border-radius: 5px;" type="submit" uk-icon="icon: close" uk-tooltip="title: {$lang['remove']}; pos: top-left"></button>
+                        </form>
+                        <script>
+                        $(function () {
+                            $('#{$result_exams["id"]}').on('submit', function (e) {
+                                e.preventDefault();
+                                $.ajax({
+                                    type: "POST",
+                                    url: "marks.php",
+                                    data: $("#{$result_exams["id"]}").serialize(),
+                                    success: function(html){ 
+                                            $('#r_$subject').html(html);
+                                    }
+                            });
+                        }); });
+                        </script>
+                        <div id="modal_{$result_exams["id"]}" uk-modal>
+                            <div class="uk-modal-dialog uk-modal-body">
+                                <form id="edit_{$result_exams["id"]}">
+                                <h2 class="uk-modal-title">{$lang['edit-exam']}</h2>
+                                <div class="uk-margin">
+                                    <label>{$lang['exam']}:</label>
+                                    <input class="uk-input" type="text" readonly value="{$result_exams["exam"]}"></input>
+                                </div>
+                                <div class="uk-margin">
+                                    <label>{$lang['mark']}:</label>
+                                    <input class="uk-input" type="number" step="0.01" min="1" max="6" placeholder="1.00" name="mark" value="{$result_exams["mark"]}" required></input>
+                                </div>
+                                <div class="uk-margin">
+                                    <label>{$lang['weight']}:</label>
+                                    <input class="uk-input" type="number" step="0.01" min="1" max="6" placeholder="1.00" name="weight" value="{$result_exams["weight"]}" required></input>
+                                </div>
+                                <div class="uk-margin">
+                                    <label>{$lang['semester']}:</label>
+                                    <select class="uk-select" name="semester">
+                                    $semester_select
+                                    </select>
+                                </div>
+                                <input type="hidden" name="id" value="{$result_exams["id"]}"><input type="hidden" name="edit"></input><input type="hidden" name="subject" value="$subject"></input>
+                                <button style="margin-bottom: 20px;" type="submit" class="uk-button uk-button-secondary" onclick="UIkit.modal(modal_{$result_exams["id"]}).hide();">{$lang['save']}</button>
+                                <button style="margin-bottom: 20px;" class="uk-modal-close uk-button uk-button-secondary" type="button">{$lang['close']}</button>
+                                </form>
+                            </div>
+                        </div>
+                        <script>
+                        $(function () {
+                            $('#edit_{$result_exams["id"]}').on('submit', function (e) {
+                                e.preventDefault();
+                                $.ajax({
+                                    type: "POST",
+                                    url: "marks.php",
+                                    data: $("#edit_{$result_exams["id"]}").serialize(),
+                                    success: function(html){ 
+                                            $('#r_$subject').html(html);
+                                            
+                                    }
+                            });
+                        }); 
+                        });
+                        </script>
+                        <button style="color:white; background-color: black; padding:5px; border-radius: 5px;" uk-icon="icon: pencil" onclick="UIkit.modal(modal_{$result_exams["id"]}).show();"  uk-tooltip="title: {$lang['edit']}; pos: top-left"></button>
+                       
+                    </td>
                 </tr>
             EOT;
             $dia_labels.="'{$result_exams["exam"]}', ";
@@ -78,6 +190,7 @@ function generateRow($subject) {
         //Diagram end
         $dia_labels.="]";
         $dia_data.="]";
+
 
     $avg_final=($avg_count>0) ? (round($avg/$avg_count, 2)) : ("?");
     $mark_table=($mark_table!="") ? ($mark_table) : ("<td colspan=\"4\"><i>No marks set yet.</i></td>");
@@ -95,6 +208,7 @@ function generateRow($subject) {
                     <th>{$lang['mark']}</th>
                     <th>{$lang['class-avg']}</th>
                     <th>{$lang['rank']}</th>
+                    <th>{$lang['actions']}</th>
                 </tr>
             </thead>
             <tbody>
@@ -125,7 +239,11 @@ function generateRow($subject) {
         <select class="uk-select" required name="exam">
             $add_select
         </select>
-        <a style="font-size:0.8em;" href="exams.php?addnew">{$lang['add-exam']}</a>
+        <a style="font-size:0.8em;" href="exams.php?addnew">{$lang['add-exam']}</a><br><br>
+        <label for="">{$lang['select-semester']}:</label>
+        <select class="uk-select" required name="semester">
+            $semester_select
+        </select>
         </div>
         <div class="uk-margin">
             <label for="">{$lang['mark']}:</label>
@@ -170,11 +288,11 @@ function generateRow($subject) {
    }
 
 //Add Mark
-if ( isset( $_POST["exam"], $_POST["mark"])) {
-    $data = array();
+if ( isset( $_POST["exam"], $_POST["mark"], $_POST["subject"], $_POST["semester"])) {
     $post_exam = $con->real_escape_string($_POST['exam']);
     $post_mark = $con->real_escape_string($_POST['mark']);
     $post_subject = $con->real_escape_string($_POST['subject']);
+    $post_semester = $con->real_escape_string($_POST['semester']);
     //Default weight
     $weight=1.0;
     $sql_add = $con->prepare("SELECT * FROM exams WHERE name = ? AND subject = ?");
@@ -183,8 +301,8 @@ if ( isset( $_POST["exam"], $_POST["mark"])) {
     $sql_add->store_result();
     if ($sql_add->num_rows > 0) {
         if($post_mark>=1.0&&$post_mark<=6.0) {
-            $sql_add_final = $con->prepare("INSERT IGNORE INTO marks (exam, username, class, subject, mark, weight) VALUES (?, ?, ?, ?, ?, ?)");
-            $sql_add_final->bind_param("ssssss", $post_exam, $_SESSION["username"], $_SESSION["class"], $post_subject, $post_mark, $weight);
+            $sql_add_final = $con->prepare("INSERT IGNORE INTO marks (exam, username, class, subject, mark, weight, semester) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            $sql_add_final->bind_param("sssssss", $post_exam, $_SESSION["username"], $_SESSION["class"], $post_subject, $post_mark, $weight, $post_semester);
             $sql_add_final->execute();
         } else {
         
@@ -194,6 +312,32 @@ if ( isset( $_POST["exam"], $_POST["mark"])) {
     echo generateRow($post_subject);
     die();
 }
+//Remove Mark
+if ( isset( $_POST["remove"], $_POST["id"], $_POST["subject"])) {
+    $post_id = $con->real_escape_string($_POST['id']);
+    $post_subject = $con->real_escape_string($_POST['subject']);
+    $sql_delete = $con->prepare("DELETE FROM marks WHERE id = ? AND username = ?");
+    $sql_delete->bind_param("ss", $post_id, $_SESSION["username"]);
+    $sql_delete->execute();
+    echo generateRow($post_subject);
+    die();
+}
+
+//Edit Mark
+if ( isset( $_POST["edit"], $_POST["id"], $_POST["mark"], $_POST["weight"], $_POST["semester"])) {
+    $post_id = $con->real_escape_string($_POST['id']);
+    $post_mark = $con->real_escape_string($_POST['mark']);
+    $post_weight = $con->real_escape_string($_POST['weight']);
+    $post_semester = $con->real_escape_string($_POST['semester']);
+    $post_subject = $con->real_escape_string($_POST['subject']);
+
+    $sql_delete = $con->prepare("UPDATE marks SET mark = ?, weight = ?, semester = ? WHERE id = ? AND username = ?");
+    $sql_delete->bind_param("sssss", $post_mark, $post_weight, $post_semester, $post_id, $_SESSION["username"]);
+    $sql_delete->execute();
+    echo generateRow($post_subject);
+    die();
+}
+
 //SELECT all subjects
 $sql_subject_select = $con->prepare("SELECT name FROM subjects");
 $sql_subject_select->execute();
