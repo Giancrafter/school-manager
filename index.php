e420f941b07ea02c9068449e93c32977d754d27b
Gian Klug <gian.klug@ict-scouts.ch>
2021-01-01 20:07:31 +0100

    Result is now based on semester

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include("config.php"); 
 include("db_connect.php");
 include("navbar.php");
-if ($stmt = $con->prepare('SELECT id, name, subject, date FROM exams WHERE class = ? ORDER BY id DESC LIMIT 3')) {
+if ($stmt = $con->prepare('SELECT id, name, subject, date FROM exams WHERE class = ? ORDER BY date DESC LIMIT 3')) {
     $stmt->bind_param('s', $_SESSION['class']);
     $stmt->execute();
     $result = $stmt->get_result();
@@ -22,37 +22,89 @@ while($row = $result->fetch_assoc()) {
     EOT;
 }
 }
-$stmt = $con->prepare("SELECT * FROM marks WHERE username = ?");
-$stmt->bind_param("s", $_SESSION['username']);
-$stmt->execute();
-$result = $stmt->get_result();
-$stmt->close();
+
+//Get Semester related things
+$semester_q = $con->prepare("SELECT semesters, start FROM classes WHERE name = ?");
+$semester_q->bind_param("s", $_SESSION["class"]);
+$semester_q->execute();
+$semester_q->bind_result($semesters, $class_start);
+$semester_q->fetch();
+$semester_q->close();
+
+//Function for counting the formated Semester up
+function countUp($formatted){
+    $parts = explode("/", $formatted);
+    if ($parts[1] == 2) {
+        $parts[1] = 1;
+        $parts[0]++;
+    } else {
+        $parts[1]++;
+    }
+    return $parts[0]."/".$parts[1];
+}
+
+//get Semester
+if (date('n')==1){
+    $currentSemester = (date('Y')-1)."/";
+} else {
+    $currentSemester = date('Y')."/";
+}
+if ((date('n')>1)&&(date('n')<8)){
+    $currentSemester.="1";
+} else {
+    $currentSemester.="2";
+}
+
+//Get subjects
 $average_total=0;
 $average_count=0;
 $points=0;
 $insufficient=0;
 $dia_labels = "";
 $dia_data = "";
+
+
+$stmt = $con->prepare("SELECT * FROM subjects");
+$stmt->execute();
+$subjects = $stmt->get_result();
+$stmt->close();
+//Fetch subjects
+while($subject = $subjects->fetch_assoc()){
+$s_avg_count = 0;
+$s_avg = 0;
+$stmt = $con->prepare("SELECT * FROM marks WHERE username = ? AND semester = ? AND subject = ?");
+$stmt->bind_param("sss", $_SESSION['username'], $currentSemester, $subject["name"]);
+$stmt->execute();
+$result = $stmt->get_result();
+$stmt->close();
+//Fetch examss for subject and current semester
 while($row = $result->fetch_assoc()) {
-//Points
-$points += round(($row["mark"]-4)*2)/2;
 
-//Insufficient Marks
-if($row["mark"]<4.0){$insufficient++;}
+$s_avg_count+=$row["weight"];
+$s_avg+=$row["mark"]*$row["weight"];
 
-//Average
-$average_count++;
-$average_total+=$row["mark"];
-
-//Diagram stuff
-$dia_labels.="'{$row["exam"]}', ";
-$dia_data.="'{$row["mark"]}', ";
+//end exams
 }
-if ($average_total==0) {
+
+
+if ($s_avg_count!= 0){
+$average_count++;
+$s_avg = (round($s_avg/$s_avg_count*2, 0))/2;
+$average_total+=$s_avg;
+//Points
+$points += $s_avg -4;
+//Insufficient Marks
+if($s_avg <4.0){$insufficient++;}
+}
+
+//End subjects
+}
+
+if ($average_count==0) {
     $average="?";
 } else {
 $average=round($average_total/$average_count,2);
-$avg_color = ($average<4.0) ? "red" : (($average>5.0) ? "green" : "grey");
+$avg_color = ($average<4.0) ? "red" : (($average>=5.0) ? "green" : "grey");
 
 $semester = $lang['okay'];
 $reasons="";
@@ -74,6 +126,20 @@ if($semester == $lang['insufficient']){
     $err_color="green";
 }
 }
+
+$stmt = $con->prepare("SELECT * FROM marks WHERE username = ? AND semester = ?");
+$stmt->bind_param("ss", $_SESSION['username'], $currentSemester);
+$stmt->execute();
+$result = $stmt->get_result();
+$stmt->close();
+//Fetch examss for subject and current semester
+while($row = $result->fetch_assoc()) {
+//Diagram stuff
+$dia_labels.="'{$row["exam"]}', ";
+$dia_data.="'{$row["mark"]}', ";
+
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -148,7 +214,13 @@ var myChart = new Chart(ctx, {
         labels: [<?=$dia_labels?>],
         datasets: [{
             label: 'Note',
-            data: [<?=$dia_data?>]
+            trendlineLinear: {
+                style: "rgb(0, 0, 0, 0.4)",
+                lineStyle: "dotted",
+                width: 1
+            },
+            data: [<?=$dia_data?>],
+
         }]
     },
     options: {
@@ -159,7 +231,13 @@ var myChart = new Chart(ctx, {
                         min: 1
                     },
                 }] },   
-    layout: {padding: { right: 15}}   
+    layout: {padding: { right: 15}},
+    responsive: true,
+    tooltips: {
+      backgroundColor: 'rgba(0, 0, 0, 1)',
+      intersect: false,
+      displayColors: false,
+    },
 }});
 myChart.options.scales.xAxes[0].display = (document.getElementById('myChart').clientHeight>=200);
 myChart.update();
